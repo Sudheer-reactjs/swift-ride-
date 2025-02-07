c58d80c260f12124ab26ef6a3f4b0e283ac89d47
Jaser Sokoli <j.sokoli@elogic.com.ua>
2025-02-07 18:41:19 +0530

    test

--- src/components/warning-text.tsx
+++ src/components/warning-text.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const WarningText = (props: any) => {
+const WarningText = (props: never) => {
     const { text } = props
     return (
         <div className="text-[#ffc725] text-sm font-semibold font-['Inter'] leading-tight">{text}</div>
