f134488cb84b7a4e2a5fbd17f27fc8b54cf896de
Jaser Sokoli <j.sokoli@elogic.com.ua>
2025-02-10 10:24:28 +0530

    docs page

--- src/app/(pages)/docs/page.tsx
+++ src/app/(pages)/docs/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import SignaturePad from '../components/SignaturePad';
+import React from "react";
+import SignaturePad from "../components/SignaturePad";
 
 const Page = () => {
-    return (
-        <div>
-         <SignaturePad />    
-        </div>
-    );
-}
+  return (
+    <div className="w-full block">
+      <div className="container !max-w-[1080px] w-full">
+         <h2 className="text-center pt-[20px] pb-[50px] lg:py-[60px]">Please Read and Sign</h2>
+        <SignaturePad />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
