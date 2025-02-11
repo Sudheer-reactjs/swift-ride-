b01d83dd8dfc3d1bd7cb2d8c4ea1a26d0c53f3a6
Jaser Sokoli <j.sokoli@elogic.com.ua>
2025-02-11 16:46:44 +0530

    fianl changes

--- src/app/(pages)/documents/page.tsx
+++ src/app/(pages)/documents/page.tsx
@@ -7,34 +7,33 @@ const Page = () => {
     const [value, setValue] = React.useState(0)
 
     return (
-        <div className="flex justify-center items-center">
-            <div className=" mx-6 md:mx-20 md:max-w-[500px]">
-                <div className="justify-start items-start gap-5 inline-flex">
-                    <div className="grow shrink basis-0 p-10 bg-[#1d2125] rounded-[10px] flex-col justify-start items-start gap-[30px] inline-flex">
+        <div className="flex justify-center items-center pt-[40px] md:pt-[60px]">
+            <div className=" container !max-w-[612px]">
+                    <div className="flex basis-0 p-[16px] md:p-10 bg-[#1d2125] rounded-[10px] flex-col justify-start items-start gap-[30px]">
                         <div className="self-stretch justify-start items-center gap-[30px] inline-flex">
                             <div className="w-[438px] flex-col justify-start items-start gap-5 inline-flex">
                                 <div className="self-stretch justify-start items-start gap-3.5 inline-flex">
-                                    <div className="grow shrink basis-0 text-[#57e667] text-3xl font-semibold font-['Inter'] leading-9">Documents</div>
+                                    <h3 className=" text-[#57e667]">Documents</h3>
                                 </div>
                             </div>
                         </div>
                         <div className="self-stretch text-white text-base font-normal font-['Inter'] leading-normal">Please remember to collect your insurance card and registration documents when picking up your vehicle. If they are lost, stolen, or you were unable to collect them, please reach out to our Dream Car partner team for assistance:</div>
-                        <div className="self-stretch h-[199px] flex-col justify-start items-start gap-6 flex">
-                            <div className="self-stretch h-[60px] flex-col justify-start items-start gap-2 flex">
-                                <div className="self-stretch text-white text-xl font-semibold font-['Inter'] leading-7">Phone</div>
+                        <div className="self-stretch flex-col justify-start items-start gap-6 flex">
+                            <div className="self-stretch flex-col justify-start items-start gap-2 flex">
+                                <div className="self-stretch text-white md:text-xl font-semibold font-['Inter'] leading-7">Phone</div>
                                 <div className="self-stretch justify-start items-center gap-2 inline-flex">
                                     <div className="grow shrink basis-0 text-white text-base font-normal font-['Inter'] leading-normal">+61 8 6119 9999</div>
                                 </div>
                             </div>
-                            <div className="self-stretch h-[60px] flex-col justify-start items-start gap-2 flex">
-                                <div className="self-stretch text-white text-xl font-semibold font-['Inter'] leading-7">Email</div>
+                            <div className="self-stretch flex-col justify-start items-start gap-2 flex">
+                                <div className="self-stretch text-white md:text-xl font-semibold font-['Inter'] leading-7">Email</div>
                                 <div className="self-stretch justify-start items-center gap-2 inline-flex">
                                     <div className="grow shrink basis-0 text-white text-base font-normal font-['Inter'] leading-normal">info@dreamcarrental.au</div>
                                 </div>
                             </div>
-                            <div className="self-stretch h-[31px] flex-col justify-start items-start gap-2 flex">
-                                <div className="self-stretch h-[31px] justify-start items-center gap-2 inline-flex">
-                                    <div className="grow shrink basis-0 h-12 justify-start items-center gap-2 flex">
+                            <div className="self-stretch  flex-col justify-start items-start gap-2 flex">
+                                <div className="self-stretch  justify-start items-center gap-2 inline-flex">
+                                    <div className="grow shrink basis-0 justify-start items-center gap-2 flex">
                                         <div className="grow shrink basis-0 text-white text-base font-normal font-['Inter'] leading-normal">Our customer service representatives are available to assist you Monday through Saturday, from 8:00 am to 6:00 pm.</div>
                                     </div>
                                 </div>
@@ -43,7 +42,6 @@ const Page = () => {
                     </div>
                 </div>
             </div>
-        </div>
 
     );
 }
--- src/app/(pages)/overview/page.tsx
+++ src/app/(pages)/overview/page.tsx
@@ -13,90 +13,90 @@ const Page = () => {
         setOpen(true)
     }
     return (
-        <div className='flex flex-col items-center'>
-            <div className='gap-8 lg:flex mx-5 mb-10 h-auto text-center xl:w-[1280px]'>
+        <div className='flex flex-col items-center pt-[40px] md:pt-[60px]'>
+            <div className='gap-8 flex flex-col md:flex-row mx-5 mb-10 h-auto text-center xl:w-[1280px]'>
                 <div className="w-full  flex-1 flex-col justify-start items-start gap-8 inline-flex ">
-                    <div className="w-full p-6 h-[235px] bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 flex">
-                        <div className="text-white  text-2xl font-semibold font-['Inter'] leading-loose">Overview</div>
+                    <div className="w-full p-6  bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 flex">
+                        <h3>Overview</h3>
                         <div className="self-stretch  justify-start items-start gap-10 inline-flex">
                             <div className="flex-col justify-between items-start inline-flex">
                                 <div className="justify-start items-center gap-[23px] inline-flex">
-                                    <Image className="w-[110px] h-[110px] rounded-full" src="/assets/prof.png" alt='' width={50} height={50} />
-                                    <div className="flex-col justify-start items-start gap-6 inline-flex">
-                                        <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">Michael Rodrigez</div>
-                                    </div>
+                                    <Image className="w-[80px] h-[80px] md:w-[110px] md:h-[110px] rounded-full" src="/assets/prof.png" alt='' width={50} height={50} />
+                                    
+                                    <h3 className="text-left">Michael Rodrigez</h3>
+                                
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="p-6 bg-[#1d2125] w-full rounded-[20px] flex-col justify-start items-start gap-8 flex">
                         <div className="self-stretch justify-between items-center inline-flex">
-                            <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">Personal Information</div>
-                            <div className="w-12 h-12 p-2.5 bg-gradient-to-r  from-[#2bdf68] to-[#b3f462] rounded-[100px] justify-center items-center flex overflow-hidden">
+                            <h3>Personal Information</h3>
+                            <div className="w-9 h-9 md:w-12 md:h-12 p-2.5 bg-gradient-to-r  from-[#2bdf68] to-[#b3f462] rounded-[100px] justify-center items-center flex overflow-hidden">
                                 <Image src='/assets/edit.svg' alt='' width={100} height={100} className='w-20 h-20' />
                             </div>
                         </div>
                         <div className="self-stretch flex-col justify-start items-start gap-8 flex">
-                            <div className="self-stretch justify-between items-center flex-wrap inline-flex">
+                            <div className="self-stretch justify-between items-center grid md:grid-cols-2 gap-3">
                                 <div className="flex-col justify-center items-start gap-2 inline-flex">
-                                    <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">First Name</div>
-                                    <div className="text-center text-white text-lg font-semibold font-['Inter'] leading-7">Michael</div>
+                                    <div className="text-center text-white text-sm leading-tight">First Name</div>
+                                    <div className="text-center text-white md:text-lg font-semibold font-['Inter'] leading-7">Michael</div>
                                 </div>
                                 <div className="flex-col justify-center items-start gap-2 inline-flex">
                                     <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">Last Name</div>
-                                    <div className="text-center text-white text-lg font-semibold font-['Inter'] leading-7">Rodrigez</div>
+                                    <div className="text-center text-white md:ext-lg font-semibold font-['Inter'] leading-7">Rodrigez</div>
                                 </div>
                             </div>
-                            <div className="self-stretch justify-between items-center flex-wrap inline-flex">
+                            <div className="self-stretch justify-between items-center grid md:grid-cols-2 gap-3">
                                 <div className="flex-col justify-center items-start gap-2 inline-flex">
                                     <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">Date of birth</div>
-                                    <div className="text-center text-white text-lg font-semibold font-['Inter'] leading-7">10.02.1996</div>
+                                    <div className="text-center text-white md:text-lg font-semibold font-['Inter'] leading-7">10.02.1996</div>
                                 </div>
                                 <div className="flex-col justify-center items-start gap-2 inline-flex">
                                     <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">Telephone</div>
-                                    <div className="text-center text-white text-lg font-semibold font-['Inter'] leading-7">+ 123 456 09700</div>
+                                    <div className="text-center text-white md:text-lg font-semibold font-['Inter'] leading-7">+ 123 456 09700</div>
                                 </div>
                             </div>
-                            <div className="self-stretch justify-between items-center flex-wrap inline-flex">
+                            <div className="self-stretch justify-between items-center grid md:grid-cols-2 gap-3">
                                 <div className="flex-col justify-start items-start gap-8 inline-flex">
                                     <div className="h-14 flex-col justify-center items-start gap-2 flex">
                                         <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">Email</div>
-                                        <div className="text-center text-white text-lg font-semibold font-['Inter'] leading-7">mail@mail.com</div>
+                                        <div className="text-center text-white md:text-lg font-semibold font-['Inter'] leading-7">mail@mail.com</div>
                                     </div>
                                 </div>
                                 <div className="flex-col justify-center items-start gap-2 inline-flex">
                                     <div className="text-center text-white text-sm font-semibold font-['Inter'] leading-tight">Documents</div>
-                                    <div className="w-[140px] h-5 rounded-3xl justify-center items-center inline-flex">
-                                        <div className="w-[140px] text-[#57e667] text-base font-semibold font-['Rubik'] leading-snug">See All</div>
+                                    <div className=" rounded-3xl  inline-flex">
+                                        <div className="text-[#57e667] text-base font-semibold  leading-snug">See All</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className={`self-stretch p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start ${!value ? 'items-center' : ''} gap-8 flex`}>
+                    <div className={`self-stretch p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start ${!value ? 'items-center' : ''} gap-8 flex `}>
                         <div className="self-stretch flex-col justify-start items-start gap-10 flex">
                             <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">Billing</div>
                             {!value ? <div className="self-stretch h-[216px] flex-col justify-center items-start gap-8 flex">
                                 <div className="self-stretch h-[76px] flex-col justify-start items-start gap-5 flex">
                                     <div className="self-stretch justify-between items-center inline-flex">
                                         <div className="text-white text-sm font-semibold font-['Inter'] leading-tight">Next bill date</div>
-                                        <div className="text-white text-lg font-semibold font-['Inter'] leading-7">Mon, October 10</div>
+                                        <div className="text-white md:text-lg font-semibold font-['Inter'] leading-7">Mon, October 10</div>
                                     </div>
                                     <div className="self-stretch h-7 flex-col justify-start items-start gap-2 flex">
                                         <div className="self-stretch justify-between items-center inline-flex">
-                                            <div className="text-white text-lg font-semibold font-['Inter'] leading-7">Visa</div>
-                                            <div className="text-white text-lg font-semibold font-['Inter'] leading-7">**9234</div>
+                                            <div className="text-white md:text-lg font-semibold font-['Inter'] leading-7">Visa</div>
+                                            <div className="text-white md:text-lg font-semibold font-['Inter'] leading-7">**9234</div>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="self-stretch h-[108px] flex-col justify-start items-start gap-4 flex">
                                     <div className="text-white text-sm font-semibold font-['Inter'] leading-tight">Receipts</div>
                                     <div className="self-stretch justify-between items-center inline-flex">
-                                        <div className="text-white text-lg font-semibold font-['Inter'] leading-7">Mon, October 10</div>
-                                        <div className="text-white text-lg font-semibold font-['Inter'] leading-7">$ 120</div>
+                                        <div className="text-white md:text-lg font-semibold font-['Inter'] leading-7">Mon, October 10</div>
+                                        <div className="text-white md:text-lg font-semibold font-['Inter'] leading-7">$ 120</div>
                                     </div>
                                     <div className="self-stretch justify-between items-center inline-flex">
-                                        <div className="text-white text-lg font-semibold font-['Inter'] leading-7">Fri, October 5</div>
+                                        <div className="text-white tmd:ext-lg font-semibold font-['Inter'] leading-7">Fri, October 5</div>
                                         <div className="text-white text-lg font-semibold font-['Inter'] leading-7">$ 120</div>
                                     </div>
                                 </div>
@@ -117,17 +117,17 @@ const Page = () => {
 
                     <div className="h-[251px] w-full p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start items-center gap-8 inline-flex">
                         <div className="self-stretch h-[129px] flex-col justify-start items-start gap-10 flex">
-                            <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">Subcription</div>
+                            <h3>Subcription</h3>
                             <div className="self-stretch h-[57px] flex-col justify-center items-start gap-8 flex">
                                 <div className="self-stretch h-[57px] flex-col justify-start items-start gap-5 flex">
                                     {!value ? <div className="self-stretch justify-between items-center inline-flex">
                                         <div className="grow shrink basis-0 flex-col justify-start items-start gap-4 inline-flex">
                                             <div className="text-white text-sm font-semibold font-['Inter'] leading-tight">Standart Plan</div>
-                                            <div className="text-white text-lg font-light font-['Rubik']">Renew 12.03.2024</div>
+                                            <div className="text-white text-lg font-light">Renew 12.03.2024</div>
                                         </div>
                                         <div className="flex-col justify-start items-start gap-2 inline-flex">
                                             <div className="self-stretch justify-between items-center inline-flex">
-                                                <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">$ 120</div>
+                                                <div className="text-white text-[20px] md:text-2xl font-semibold font-['Inter'] leading-loose">$ 120</div>
                                             </div>
                                         </div>
                                     </div>
@@ -145,9 +145,9 @@ const Page = () => {
 
                     <div className={`p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start ${!value ? 'items-center' : ''} gap-8 inline-flex w-full`} >
                         <div className="self-stretch flex-col justify-start items-start gap-10 flex">
-                            <div className="text-white text-2xl font-semibold font-['Inter'] leading-loose">Current Vehicle</div>
+                            <h3>Current Vehicle</h3>
                             {!value ? <>
-                                <Image className="w-full h-[297px] object-contain" src="/assets/white-car.png" alt='' width={50} height={50} />
+                                <Image className="w-full h-[297px] object-contain"  layout="intrinsic" src="/assets/white-car.png"  alt='' width={576} height={297} />
                                 <div className="self-stretch h-7 flex-col justify-center items-start gap-8 flex">
                                     <div className="self-stretch justify-center items-center gap-5 inline-flex">
                                         <div className="grow shrink basis-0 flex-col justify-center items-center gap-4 inline-flex">
@@ -159,7 +159,7 @@ const Page = () => {
                                 <WarningText text='No active subscriptions' />}
                         </div>
                         {!value ? <div className="h-[58px] px-5 w-full pt-[19px] pb-[18px] rounded-[100px] border-2 border-white justify-center items-center flex">
-                            <div className="w-full text-center text-white text-lg font-black font-['HeliosBlackC']">Change Vehicle</div>
+                            <div className="w-full text-center text-white text-lg ">Change Vehicle</div>
                         </div>
                             :
                             <div className="w-[292px] h-[42px] px-5 py-[9px] bg-gradient-to-r from-[#2bdf68] to-[#b3f462] rounded-3xl justify-center flex">
@@ -169,9 +169,9 @@ const Page = () => {
                     </div>
                     <div className="w-full self-stretch justify-center items-start inline-flex">
                         <div className="w-full flex flex-col justify-start items-start gap-8">
-                            <div className="w-full p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 inline-flex">
+                            <div className="w-full p-4 md:p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 inline-flex">
                                 <div className="justify-start items-center gap-8 inline-flex">
-                                    <div className="text-white text-lg  xl:text-2xl font-semibold font-['Inter'] leading-loose">
+                                    <div className="text-white text-left text-lg  xl:text-2xl font-semibold font-['Inter'] leading-loose">
                                         Driver’s License Verification
                                     </div>
                                     <div className={`w-[148px] h-[42px] px-5 py-[9px] ${!value ? 'bg-[#43fc47]/40' : 'bg-[#e3d734]/95'} rounded-3xl  justify-center items-center flex`}>
@@ -194,7 +194,7 @@ const Page = () => {
                                         shouldCloseOnOverlayClick={true}
                                     >
                                         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 border-2">
-                                            <div className="bg-[#191919] max-w-[520px] text-white shadow-lg p-6 w-full mx-auto relative rounded-xl flex flex-col border-[#81e276] border-4">
+                                            <div className="bg-[#191919] max-w-[520px] w-[94%] text-white shadow-lg p-6  mx-auto relative rounded-xl flex flex-col border-[#81e276] border-4">
                                                 {/* Close Button */}
                                                 <button onClick={() => setOpen(false)} className="absolute top-3 right-3 text-white hover:text-gray-300 text-xl">
                                                     &times;
@@ -223,9 +223,9 @@ const Page = () => {
                                     </Modal>
                                 }
                             </div>
-                            <div className="w-full p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 inline-flex">
+                            <div className="w-full p-4 md:p-6 bg-[#1d2125] rounded-[20px] flex-col justify-start items-start gap-8 inline-flex">
                                 <div className="justify-start items-center gap-8 inline-flex">
-                                    <div className="text-white text-lg xl:text-2xl font-semibold font-['Inter'] leading-loose">
+                                    <div className="text-white text-left text-lg xl:text-2xl font-semibold font-['Inter'] leading-loose">
                                         Driver’s License Verification
                                     </div>
                                     <div className={`w-[148px] h-[42px] px-5 py-[9px] ${!value ? 'bg-[#43fc47]/40' : 'bg-[#e3d734]/95'} rounded-3xl  justify-center items-center flex`}>
@@ -233,7 +233,7 @@ const Page = () => {
                                     </div>
                                 </div>
                                 {
-                                    value ? <div className="w-[383px] h-[42px] px-5 py-[9px] bg-gradient-to-r from-[#2bdf68] to-[#b3f462] rounded-3xl border justify-center items-center inline-flex">
+                                    value ? <div className="w-[383px] h-[42px] px-5 py-[9px] bg-gradient-to-r from-[#2bdf68] to-[#b3f462] rounded-3xl justify-center items-center inline-flex">
                                         <div className="w-[343px] text-center text-[#181818] text-base font-medium font-['Inter'] leading-normal">Upload Rideshare Account Screenshot</div>
                                     </div>
                                         : ''
